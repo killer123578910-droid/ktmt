d7987f617f33ba482fb5cccbbcfd230757418df7
Khuong <killer123578910@gmail.com>
2026-04-16 23:13:14 +0700

    baitap

--- 9.asm
+++ 9.asm
@@ -2,7 +2,7 @@
 .stack 100h
 .data
       crlf db 13,10,"$"
-      ex db "nhap sai roi!$"
+      ex db 10,"nhap sai roi!$"
       x dw ?
       y dw ?
       
@@ -22,10 +22,11 @@ endl endp
 
 nhapso proc
     mov bx,2
+    mov cx,0
 lap:
     mov ah,1
     int 21h
-    
+    inc cx
     cmp al,"#"
     je retta
     cmp al,"0"
@@ -33,16 +34,18 @@ lap:
     cmp al,"1"
     jg rett
     
-    sub ah,'0'                             
     mov ah,0
+    sub ax,'0'                             
     mov y,ax
-    
+  
     mov ax,x
     
     mul bx
     add ax,y
     
     mov x,ax
+    cmp cx,8
+    je retta
     jmp lap
     
     
@@ -55,9 +58,35 @@ rett:
 nhapso endp
 
 inso proc
+    mov bx, 16
+    mov cx,0
+    mov ax,x
+lap1:
+    mov dx,0
+    div bx
+  
+    cmp dx,10
+    jl digit
     
-    
-    
+    add dx,"A"
+    sub dx,10
+    jmp skip
+  
+  digit:
+    add dx,"0"  
+  skip:
+    push dx
+    inc cx                                             
+    cmp ax,0
+    jg lap1
+inre:
+    pop dx
+    mov ah,2
+    int 21h
+    loop inre
+        
+ret
+inso endp
     
     
     
@@ -73,8 +102,19 @@ main proc
     mov ax,@data
     mov ds,ax
     
+    call nhapso
+    
+    cmp bx,0
+    je thoat
+    
+    call endl
+    call inso
+    jmp ene
     
     
+thoat:
+    call exec
+ ene: 
     mov ah, 4ch
     int 21h
     
